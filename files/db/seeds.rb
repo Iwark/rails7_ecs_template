b23e8a2f8e010a605d80908811534da6e5e19cea
Kohei Iwasaki <iwark02@gmail.com>
2024-02-02 11:37:46 +0900

    Use AWS role-to-assume instead of access keys

--- files/db/seeds.rb
+++ files/db/seeds.rb
@@ -6,7 +6,7 @@ end
 Dir.glob("#{Rails.root}/db/seeds/*.yml").each do |yaml_filename|
   target_model = File.basename(yaml_filename.split('-').last,".yml").classify.constantize
   File.open(yaml_filename) do |load_target_yaml|
-    records = YAML.unsafe_load(load_target_yaml)
+    records = YAML.unsafe_load(load_target_yaml) || []
 
     records.each do |record|
       target_model.create!(record)
